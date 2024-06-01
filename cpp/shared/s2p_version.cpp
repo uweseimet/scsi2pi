23c6077119325bedc39963cb99cc28591f9c0103
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-06-01 10:41:53 +0200

    Set version to next development cycle

--- cpp/shared/s2p_version.cpp
+++ cpp/shared/s2p_version.cpp
@@ -9,6 +9,6 @@
 #include "s2p_version.h"
 
 const int s2p_major_version = 3;
-const int s2p_minor_version = 3;
+const int s2p_minor_version = 4;
 const int s2p_revision = 0;
 const std::string s2p_suffix = "-devel";
