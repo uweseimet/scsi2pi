21fc0a0195e88b83867db058b7fa085e2f4567a6
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-10-10 15:44:32 +0200

    Update logging

--- cpp/devices/tape.cpp
+++ cpp/devices/tape.cpp
@@ -141,9 +141,6 @@ int Tape::GetVariableBlockSize()
 {
     const int length = FindNextObject(object_type::BLOCK, 0);
 
-    LogTrace(fmt::format("Next block size is {} byte(s), chunk size is {} byte(s)", length,
-            GetController()->GetChunkSize()));
-
     if (length != GetController()->GetChunkSize()) {
         // In Fixed mode an incorrect block length always results in an error
         if (GetController()->GetCdb()[1] & 0x01) {
