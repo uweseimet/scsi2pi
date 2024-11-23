20b332938451c7ba7f021c8dff45a96b37da9337
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-11-23 09:20:53 +0100

    Update sense data output

--- cpp/shared/s2p_util.cpp
+++ cpp/shared/s2p_util.cpp
@@ -239,13 +239,12 @@ string s2p_util::FormatSenseData(span<const byte> sense_data)
     const string &s = FormatSenseData(static_cast<sense_key>(sense_data[2] & byte { 0x0f }),
         static_cast<asc>(sense_data[12]), static_cast<int>(sense_data[13]));
 
-    if ((sense_data[0] & byte { 0x80 }) != byte { 0 }) {
-        return s
-            + fmt::format(", ILI: {0}, INFORMATION: {1}", (sense_data[2] & byte { 0x20 }) != byte { 0 } ? "1" : "0",
-                GetInt32(sense_data, 3));
+    if ((sense_data[0] & byte { 0x80 }) == byte { 0 }) {
+        return s;
     }
 
-    return s;
+    return s + fmt::format(", ILI: {0}, INFORMATION: {1}", (sense_data[2] & byte { 0x20 }) != byte { 0 } ? "1" : "0",
+        GetInt32(sense_data, 3));
 }
 
 string s2p_util::FormatSenseData(sense_key sense_key, asc asc, int ascq)
