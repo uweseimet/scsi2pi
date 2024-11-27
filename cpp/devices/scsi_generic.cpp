a258e7576f1f6c63407550642f7b13c6f3de0a7b
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-11-27 13:33:24 +0100

    Testing

---
+++ cpp/devices/scsi_generic.cpp
@@ -0,0 +1,135 @@
+//---------------------------------------------------------------------------
+//
+// SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
+//
+// Copyright (C) 2024 Uwe Seimet
+//
+// Implementation of a generic SCSI device, using the Linux SG driver
+//
+//---------------------------------------------------------------------------
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <libudev.h>
+#include <sys/ioctl.h>
+#include <scsi/sg.h>
+#include "buses/bus_factory.h"
+#include "shared/s2p_exceptions.h"
+#include "scsi_generic.h"
+
+using namespace s2p_util;
+
+ScsiGeneric::ScsiGeneric(int lun) : PrimaryDevice(SCSG, scsi_level::scsi_2, lun)
+{
+    SetProduct("SCSI GENERIC");
+    SupportsParams(true);
+    SetReady(true);
+}
+
+bool ScsiGeneric::SetUp()
+{
+    device = GetParam(DEVICE);
+    if (device.empty()) {
+        LogError(fmt::format("Missing device file parameter"));
+        return false;
+    }
+
+    if (!GetAsUnsignedInt(GetParam(TIMEOUT), timeout) || !timeout) {
+        LogError(fmt::format("Invalid timeout value '{}'", GetParam(TIMEOUT)));
+        return false;
+    }
+
+    fd = open(device.c_str(), O_RDWR | O_NONBLOCK);
+    if (fd == -1) {
+        LogError(fmt::format("Can't open '{0}': {1}", device, strerror(errno)));
+        return false;
+    }
+
+    return true;
+}
+
+void ScsiGeneric::CleanUp()
+{
+    if (fd != -1) {
+        close(fd);
+    }
+}
+
+void ScsiGeneric::Dispatch(scsi_command cmd)
+{
+    count = BusFactory::Instance().GetCommandBytesCount(cmd);
+    if (!count) {
+        LogTrace(fmt::format("Received unsupported command: ${:02x}", static_cast<int>(cmd)));
+        throw scsi_exception(sense_key::illegal_request, asc::invalid_command_operation_code);
+    }
+
+    // TODO List with allocation length offsets for the known commands
+    GetController()->SetTransferSize(512, 512);
+
+    GetController()->SetCurrentLength(512);
+    DataInPhase(ReadData(GetController()->GetBuffer()));
+}
+
+param_map ScsiGeneric::GetDefaultParams() const
+{
+    return {
+        {   DEVICE, ""},
+        {   TIMEOUT, "3"}
+    };
+}
+
+vector<uint8_t> ScsiGeneric::InquiryInternal() const
+{
+    assert(false);
+    return {};
+}
+
+int ScsiGeneric::ReadData(span<uint8_t> buf)
+{
+    assert(count);
+
+    sg_io_hdr io_hdr = { };
+
+    io_hdr.interface_id = 'S';
+
+    if (GetController()->GetRemainingLength()) {
+        io_hdr.dxfer_direction =
+            WRITE_COMMANDS.contains(static_cast<scsi_command>(GetController()->GetCdb()[0])) ?
+                SG_DXFER_TO_DEV : SG_DXFER_FROM_DEV;
+    }
+    else {
+        io_hdr.dxfer_direction = SG_DXFER_NONE;
+    }
+
+    io_hdr.dxfer_len = GetController()->GetRemainingLength();
+    io_hdr.dxferp = io_hdr.dxfer_len ? buf.data() : nullptr;
+
+    array<uint8_t, 13> sense_data = { };
+    io_hdr.sbp = sense_data.data();
+    io_hdr.mx_sb_len = sense_data.size();
+
+    vector<uint8_t> cdb;
+    for (int i = 0; i < count; i++) {
+        cdb.push_back(GetController()->GetCdb()[i]);
+    }
+    io_hdr.cmdp = cdb.data();
+    io_hdr.cmd_len = cdb.size();
+
+    io_hdr.timeout = timeout * 1000;
+
+    int status = ioctl(fd, SG_IO, &io_hdr) < 0 ? -1 : io_hdr.status;
+    if (!status && sense_data[2] & 0x0f) {
+        status = sense_data[2] & 0x0f;
+    }
+
+    if (status) {
+        throw scsi_exception(static_cast<enum sense_key>(sense_data[2] & 0x0f), static_cast<enum asc>(sense_data[12]));
+    }
+
+    return GetController()->GetRemainingLength() - io_hdr.resid;
+}
+
+void ScsiGeneric::WriteData(span<const uint8_t>, scsi_command, int)
+{
+    assert(false);
+}
---
+++ cpp/devices/scsi_generic.h
@@ -0,0 +1,53 @@
+//---------------------------------------------------------------------------
+//
+// SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
+//
+// Copyright (C) 2024 Uwe Seimet
+//
+// Implementation of a SCSI printer (see SCSI-2 specification for a command description)
+//
+//---------------------------------------------------------------------------
+#pragma once
+
+#include "base/primary_device.h"
+
+using namespace std;
+
+class ScsiGeneric : public PrimaryDevice
+{
+
+public:
+
+    explicit ScsiGeneric(int);
+    ~ScsiGeneric() override = default;
+
+    bool SetUp() override;
+    void CleanUp() override;
+
+    void Dispatch(scsi_command) override;
+
+    param_map GetDefaultParams() const override;
+
+    vector<uint8_t> InquiryInternal() const override;
+
+    int ReadData(span<uint8_t>) override;
+    void WriteData(span<const uint8_t>, scsi_command, int) override;
+
+private:
+
+    string device;
+
+    int count = 0;
+
+    int timeout = 0;
+
+    int fd = -1;
+
+    inline static const unordered_set<scsi_command> WRITE_COMMANDS = { scsi_command::write_6, scsi_command::write_10,
+        scsi_command::write_16, scsi_command::verify_10, scsi_command::verify_16, scsi_command::write_long_10,
+        scsi_command::write_long_16, scsi_command::mode_select_6, scsi_command::mode_select_10,
+        scsi_command::execute_operation };
+
+    static constexpr const char *DEVICE = "device";
+    static constexpr const char *TIMEOUT = "timeout";
+};
