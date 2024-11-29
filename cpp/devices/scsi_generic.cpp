4e6010efd9b5ac0391d92eae3b6cf9922ff04673
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-11-29 22:07:31 +0100

    Revert "Remove unused code"
    This reverts commit 7548f8d354b1d0a0f5f6b7984c1221467dba450c.

--- cpp/devices/scsi_generic.cpp
+++ cpp/devices/scsi_generic.cpp
@@ -54,7 +54,11 @@ void ScsiGeneric::CleanUp()
 
 void ScsiGeneric::Dispatch(scsi_command cmd)
 {
-    // Commands not known by s2p have already been rejected by the controller
+    count = BusFactory::Instance().GetCommandBytesCount(cmd);
+    if (!count) {
+        LogTrace(fmt::format("Received unsupported command: ${:02x}", static_cast<int>(cmd)));
+        throw scsi_exception(sense_key::illegal_request, asc::invalid_command_operation_code);
+    }
 
     const int allocation_length = BusFactory::Instance().GetAllocationLength(GetController()->GetCdb());
 
