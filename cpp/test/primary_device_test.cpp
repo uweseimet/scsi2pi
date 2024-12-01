5281151ded982ebcb0fd3798da927c30efa7138d
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-12-01 21:24:04 +0100

    Update unit test

--- cpp/test/primary_device_test.cpp
+++ cpp/test/primary_device_test.cpp
@@ -300,11 +300,13 @@ TEST(PrimaryDeviceTest, RequestSense)
 
     const auto &data = controller->GetBuffer();
 
-    // DESC
-    controller->SetCdbByte(1, 0x01);
     // ALLOCATION LENGTH
     controller->SetCdbByte(4, 255);
-    Dispatch(*device, scsi_command::request_sense, sense_key::illegal_request, asc::invalid_field_in_cdb);
+
+    // DESC
+    controller->SetCdbByte(1, 0x01);
+    TestShared::Dispatch(*device, scsi_command::request_sense, sense_key::illegal_request, asc::invalid_field_in_cdb);
+    controller->SetCdbByte(1, 0x00);
 
     device->SetReady(false);
     TestShared::Dispatch(*device, scsi_command::request_sense, sense_key::not_ready, asc::medium_not_present);
