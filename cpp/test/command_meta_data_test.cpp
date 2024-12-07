5751dc9c2858df3c03dc72599e25ded1a043e303
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-12-07 18:05:48 +0100

    Add unit test

--- cpp/test/command_meta_data_test.cpp
+++ cpp/test/command_meta_data_test.cpp
@@ -144,3 +144,11 @@ TEST(CommandMetaDataTest, GetCommandName)
     EXPECT_EQ("RECEIVE OPERATION RESULTS (SCSI2Pi-specific)",
         meta_data.GetCommandName(scsi_command::receive_operation_results));
 }
+
+TEST(CommandMetaDataTest, LogCdb)
+{
+
+    const array<const uint8_t, 6> &cdb = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+    EXPECT_EQ("type is executing REZERO/REWIND, CDB 01:02:03:04:05:06",
+        CommandMetaData::Instance().LogCdb(cdb, "type"));
+}
