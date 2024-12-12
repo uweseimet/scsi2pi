d7426c42b400736b81d56a20c23ee8f21557f9e3
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-12-13 00:42:57 +0100

    Update unit test

--- cpp/test/scsi_generic_test.cpp
+++ cpp/test/scsi_generic_test.cpp
@@ -9,11 +9,12 @@
 #ifdef __linux__
 
 #include <gtest/gtest.h>
+#include "test_shared.h"
 #include "devices/scsi_generic.h"
 
 TEST(ScsiGenericTest, Device_Defaults)
 {
-    ScsiGeneric device(0);
+    ScsiGeneric device(0, "");
 
     EXPECT_EQ(SCSG, device.GetType());
     EXPECT_FALSE(device.SupportsImageFile());
@@ -26,15 +27,17 @@ TEST(ScsiGenericTest, Device_Defaults)
     EXPECT_FALSE(device.IsLocked());
     EXPECT_FALSE(device.IsStoppable());
     EXPECT_FALSE(device.IsStopped());
+
+    EXPECT_EQ("SCSI2Pi", device.GetVendor());
+    EXPECT_EQ("SG 3 DEVICE", device.GetProduct());
+    EXPECT_EQ(testing::TestShared::GetVersion(), device.GetRevision());
 }
 
-TEST(ScsiGenericTest, GetDefaultParams)
+TEST(ScsiGenericTest, GetDevice)
 {
-    ScsiGeneric device(0);
+    ScsiGeneric device(0, "device");
 
-    const auto &params = device.GetDefaultParams();
-    EXPECT_EQ(1U, params.size());
-    EXPECT_EQ("", params.at("device"));
+    EXPECT_EQ("device", device.GetDevice());
 }
 
 #endif
