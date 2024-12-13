29f6eec0152974925fb98bc6dc115a30841d723a
Uwe Seimet <Uwe.Seimet@seimet.de>
2024-12-13 22:47:55 +0100

    Add missing files

---
+++ cpp/shared/sg_adapter.cpp
@@ -0,0 +1,77 @@
+//---------------------------------------------------------------------------
+//
+// SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
+//
+// Copyright (C) 2024 Uwe Seimet
+//
+//---------------------------------------------------------------------------
+
+#include "sg_adapter.h"
+#include <array>
+#include <iostream>
+#include <fcntl.h>
+#include <scsi/sg.h>
+#include <sys/ioctl.h>
+#include <spdlog/spdlog.h>
+#include "shared/command_meta_data.h"
+#include "shared/s2p_exceptions.h"
+
+using namespace spdlog;
+
+string SgAdapter::Init(const string &device)
+{
+    if (!device.starts_with("/dev/sg")) {
+        return fmt::format("Missing or invalid device file: '{}'", device);
+    }
+
+    fd = open(device.c_str(), O_RDWR | O_NONBLOCK);
+    if (fd == -1) {
+        return fmt::format("Can't open '{0}': {1}", device, strerror(errno));
+    }
+
+    if (int v; ioctl(fd, SG_GET_VERSION_NUM, &v) < 0 || v < 30000) {
+        CleanUp();
+        return fmt::format("{0} does not appear to be a Linux SG 3 driver device: {1}", device, strerror(errno));
+    }
+
+    return "";
+}
+
+void SgAdapter::CleanUp()
+{
+    if (fd != -1) {
+        close (fd);
+        fd = -1;
+    }
+}
+
+SgAdapter::SgResult SgAdapter::SendCommand(span<uint8_t> cdb, span<uint8_t> buf, int length, int timeout,
+    bool write) const
+{
+    sg_io_hdr io_hdr = { };
+
+    io_hdr.interface_id = 'S';
+
+    if (length) {
+        io_hdr.dxfer_direction = write ? SG_DXFER_TO_DEV : SG_DXFER_FROM_DEV;
+    }
+    else {
+        io_hdr.dxfer_direction = SG_DXFER_NONE;
+    }
+
+    io_hdr.dxfer_len = length;
+    io_hdr.dxferp = io_hdr.dxfer_len ? buf.data() : nullptr;
+
+    array<uint8_t, 18> sense_data = { };
+    io_hdr.sbp = sense_data.data();
+    io_hdr.mx_sb_len = sense_data.size();
+
+    io_hdr.cmdp = cdb.data();
+    io_hdr.cmd_len = static_cast<uint8_t>(cdb.size());
+
+    io_hdr.timeout = timeout * 1000;
+
+    const int status = ioctl(fd, SG_IO, &io_hdr) < 0 ? -1 : io_hdr.status;
+
+    return {status, io_hdr.resid, static_cast<sense_key>(static_cast<int>(sense_data[2]) & 0x0f)};
+}
---
+++ cpp/shared/sg_adapter.h
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+//
+// SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
+//
+// Copyright (C) 2024 Uwe Seimet
+//
+//---------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstdint>
+#include <span>
+#include <string>
+#include "shared/scsi.h"
+
+using namespace std;
+
+class SgAdapter
+{
+
+public:
+
+    using SgResult = struct _SgResult {
+        _SgResult(int s, int l, sense_key k) : status(s), length(l), key(k) {}
+
+        int status;
+        int length;
+        sense_key key;
+    };
+
+    string Init(const string&);
+    void CleanUp();
+
+    SgResult SendCommand(span<uint8_t>, span<uint8_t>, int, int, bool) const;
+
+private:
+
+    int fd = -1;
+};
