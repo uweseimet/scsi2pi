bab762df30e873c20aa52620606e7df6ccc2cdf1
Uwe Seimet <Uwe.Seimet@seimet.de>
2025-01-06 20:30:08 +0100

    Update version for first RC

--- cpp/shared/s2p_version.cpp
+++ cpp/shared/s2p_version.cpp
@@ -2,7 +2,7 @@
 //
 // SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
 //
-// Copyright (C) 2023-2024 Uwe Seimet
+// Copyright (C) 2023-2025 Uwe Seimet
 //
 //---------------------------------------------------------------------------
 
@@ -11,4 +11,4 @@
 const int s2p_major_version = 5;
 const int s2p_minor_version = 0;
 const int s2p_revision = 0;
-const std::string s2p_suffix = "-devel";
+const std::string s2p_suffix = "-rc1";
