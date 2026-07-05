34036d67522405f84720945ecd3475852ffa3288
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-07-05 20:43:00 +0200

    Add special handling of 0 for READ/WRITE (6) to SCSI-to-USB bridge

--- cpp/test/abstract_controller_test.cpp
+++ cpp/test/abstract_controller_test.cpp
@@ -32,7 +32,6 @@ TEST(AbstractControllerTest, SetCurrentLength)
 {
     MockAbstractController controller;
 
-    EXPECT_EQ(512U, controller.GetBuffer().size());
     controller.SetCurrentLength(1);
     EXPECT_LE(1U, controller.GetBuffer().size());
     controller.SetCurrentLength(10000);
