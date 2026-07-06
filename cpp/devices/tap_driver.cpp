457ed4b43478e13fc9ff42135d3de7a5efc39c80
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-07-06 09:15:44 +0200

    Increase garbage buffer size

--- cpp/devices/tap_driver.cpp
+++ cpp/devices/tap_driver.cpp
@@ -322,7 +322,7 @@ string TapDriver::BrSetIf(int fd, const string &interface, bool add)
 void TapDriver::Flush(logger &logger) const
 {
     while (HasPendingPackets()) {
-        array<uint8_t, ETH_FRAME_LEN> m_garbage_buffer;
+        array<uint8_t, ETH_FRAME_LEN + 4> m_garbage_buffer;
         static_cast<void>(Receive(m_garbage_buffer, logger));
     }
 }
