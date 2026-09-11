2d3a561d479b21efdff849f0d571317e84de143b
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-09-11 10:15:44 +0200

    Fix Sonar issue

--- cpp/shared/s2p_util.cpp
+++ cpp/shared/s2p_util.cpp
@@ -32,7 +32,7 @@ tuple<int, int, string> GetPwData()
     const int uid = sudo_user ? stoi(sudo_user) : s2p_util::GetEuid();
 
     passwd pwd = { };
-    passwd *p_pwd;
+    passwd *p_pwd = nullptr;
 
     if (array<char, 256> pwbuf; uid != -1 && !getpwuid_r(uid, &pwd, pwbuf.data(), pwbuf.size(), &p_pwd) && p_pwd) {
         if (error_code error; exists(s2p_util::DEFAULT_APP_FOLDER, error)) {
