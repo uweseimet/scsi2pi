c403535312fea878e90461d42ea52b9774f4e8db
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-09-20 09:28:12 +0200

    Update message processing and reset handling

--- cpp/controllers/controller.cpp
+++ cpp/controllers/controller.cpp
@@ -28,6 +28,9 @@ void Controller::Reset()
 
     identified_lun = -1;
 
+    deferred_sense_key = NO_SENSE;
+    deferred_asc = NO_ADDITIONAL_SENSE_INFORMATION;
+
     ResetFlags();
 }
 
@@ -640,6 +643,16 @@ void Controller::ProcessMessage()
     if (atn_msg) {
         atn_msg = false;
         ParseMessage();
+        msg_bytes.clear();
+
+        if (IsBusFree()) {
+            return;
+        }
+
+        if (IsMsgIn()) {
+            atn_msg = true;
+            return;
+        }
     }
 
     Command();
