ee721f93382bf575e6ca74ba9fa5851f13f62bf8
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-09-24 17:08:24 +0200

    Update parser and unit tests

---
+++ cpp/test/initiator_executor_test.cpp
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+//
+// SCSI2Pi, SCSI device emulator and SCSI tools for the Raspberry Pi
+//
+// Copyright (C) 2026 Uwe Seimet
+//
+//---------------------------------------------------------------------------
+
+#include "mocks.h"
+#include "initiator/initiator_executor.h"
+
+TEST(InitiatorExecutorTest, SetLimit)
+{
+    MockBus bus;
+    InitiatorExecutor executor(bus, 0, *default_logger());
+
+    EXPECT_TRUE(executor.SetLimit(1));
+    EXPECT_TRUE(executor.SetLimit(0));
+    EXPECT_FALSE(executor.SetLimit(-1));
+}
